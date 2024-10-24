d2857731bdd26bbaf466a8e71923d548b2234c2d
leona <leona@192.168.0.7>
2024-10-24 15:33:41 -0300

    Subiendo subi hola mundo ahre

--- src/practicando/Practicando.java
+++ src/practicando/Practicando.java
@@ -3,7 +3,7 @@ package practicando;
 public class Practicando {
 
     public static void main(String[] args) {
-        
+        System.out.println("Ahora quiero cambiar, que haces??¿¿¿");
         
     }
     
